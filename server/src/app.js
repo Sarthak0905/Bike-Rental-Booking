bc5ef0bd825e438c06f6ae55faaab00a9e15fbe3
sarthak nagpure <sarthaknagpure1@gmail.com>
2026-07-07 13:47:16 +0530

    add cors Client Url

--- server/src/app.js
+++ server/src/app.js
@@ -34,10 +34,13 @@ const adminRoutes = require("./routes/adminRoutes");
 
 const app = express();
 app.use(helmet());
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CLIENT_URL,
-    credentials: true
-  }));
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(
