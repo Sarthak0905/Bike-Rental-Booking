e6bd23966cd24bfc59f28b989be8b2c5315017d4
sarthak nagpure <sarthaknagpure1@gmail.com>
2026-08-20 01:43:58 +0530

    feat: switch nodemailer to use google oauth2

--- server/src/config/email.js
+++ server/src/config/email.js
@@ -1,12 +1,13 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT),
-  secure: false,
+  service: "gmail",
   auth: {
+    type: "OAuth2",
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN
   }
 });
 
